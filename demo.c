9af7d658474ea576f55d784535f9f6db4a782a61
mahir-yadav <mahir21263@iiitd.ac.in>
2022-11-21 01:19:51 +0530

    update1

--- demo.c
+++ demo.c
@@ -1,26 +1,21 @@
-// Name - Harjeet Singh Yadav
-// Roll Number - 2020561
-// CSAI
-
 #include <unistd.h>
 #include <sys/syscall.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
 
-#define SYS_matrix_copy 550  // 550 is my syscall id, change 
-//this no according to your sycall id ( number which you gave it in the table )
+#define SYS_matrix_copy 451
 
 
 int main(int argc, char **argv)
 {
   
-    float matrix1[2][3] = { {1.23, 2.56, 4.89 }, {3.123, 4.568, 8.235 } };
-    float matrix2[2][3];
+    float matrix1[2][2] = { {1.98, 2.93 }, {3.23, 4.33} };
+    float matrix2[2][2];
  
     printf("Making system call with matrix1 and matrix 2\n");
 
-    long res = syscall(SYS_matrix_copy, matrix1, matrix2, 2, 3);
+    long res = syscall(SYS_matrix_copy, matrix1, matrix2);
 
     printf("System call returned %ld.\n", res);
     perror("Console Output");
@@ -29,9 +24,9 @@ int main(int argc, char **argv)
 
     for(int i = 0 ; i< 2 ; i++){
 
-        for(int j =0 ; j<3 ; j++){
+        for(int j =0 ; j<2 ; j++){
 
-            printf("%d ",matrix2[i][j]);
+            printf("%f ",matrix2[i][j]);
         }
 
         printf("\n");
